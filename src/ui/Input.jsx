99b69721aefeac7368ac4678a3ec8186e476697c
Adebem <adeolabusayo1995@gmail.com>
2025-06-04 07:11:15 +0100

    update the UI

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 const Input = styled.input`
   border: 1px solid #ddd;
+
   border-radius: var(--border-radius-md);
   padding: 0.8rem 1.2rem;
-  margin: 2rem;
+  margin: 1.5rem 0;
 `;
 
 export default Input;
